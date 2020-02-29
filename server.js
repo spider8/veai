6a245e8f578c2e2566c63de14f3e359d282e1f1e
Anderson Silva <anderson.araujo.08@gmail.com>
2020-02-29 14:34:48 -0300

    Fix volumes from docker to enable nodemon

--- server.js
+++ server.js
@@ -2,20 +2,32 @@
 
 const express = require('express');
 //const router = express.Router();  
-const mongoose = require('mongoose');  
-mongoose.connect('mongodb://localhost:27017');  
+const mongoose = require('mongoose');
+
+const server = 'mongodb:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'movie';      // REPLACE WITH YOUR DB NAME
+
+mongoose.connect(`mongodb://${server}/${database}`, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection;
+
+db.on('error', error => console.log("AZEDOU", error));
+
+db.once('open', function () {
+  console.log('Conectado ao MongoDB.')
+});
+
 const Schema = mongoose.Schema;
 
 // Constants
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || '0.0.0.0';
 
-var movieSchema = new Schema({  
-  name: {type: String, required: true},  
-  year: Number,   
- }, {collection: 'movies'});  
- 
- const movie = mongoose.model('Movie', movieSchema);  
+var movieSchema = new Schema({
+  name: { type: String, required: true },
+  year: Number,
+}, { collection: 'movies' });
+
+const movie = mongoose.model('Movie', movieSchema);
 
 // App
 const app = express();
