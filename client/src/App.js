be2159c82dc75f687edd5a5bb3bec6da95ef9e00
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-03 20:20:23 -0300

    Add handle search in SearchBar component.

--- client/src/App.js
+++ client/src/App.js
@@ -9,19 +9,31 @@ import SearchBar from "./Components/SearchBar";
 import WrapContent from "./Components/WrapContent";
 import FabButton from "./Components/FabButton";
 import AddMovie from "./Components/AddMovie";
-import SearchBox from './Components/SearchBox'
+import SearchBox from "./Components/SearchBox";
+import PosterMovie from "./Components/PosterMovie";
 
 function App() {
   const [showStory, setShowStory] = useState(false);
   const [movies, setMovies] = useState([]);
+
+  async function SearchMovie(value) {
+    const movies = await axios.get(`http://localhost:80/movie?search=${value}`);
+    setMovies(movies.data);
+  }
 
   return (
     <Container>
       <Header />
       <WrapContent>
         {showStory && <StoryCard />}
-        <SearchBar />
-        {!showStory && <SearchBox>Oi</SearchBox>}
+        <SearchBar submit={SearchMovie} />
+        {!showStory && (
+          <SearchBox>
+            {movies.map(movie => (
+              <PosterMovie key={movie.id} url={movie.poster_path} />
+            ))}
+          </SearchBox>
+        )}
       </WrapContent>
       <FabButton>
         <AddMovie />
