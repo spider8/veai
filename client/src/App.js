65df209e86d04f3730498459e3a23af696838498
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-03 00:08:07 -0300

    Add axios dep and basic search bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+//import {useQuery} from "react-query"
+import axios from "axios";
+
+//function getMovies(string){}
 
 function App() {
+  //const {data, loading, error} = useQuery("movies" )
+  const [list, setList] = useState([]);
+  const [search, setSearch] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form >
+        <input name="search" onChange={e => setSearch(e.target.value)} value={search} />
+        <input type="submit" onClick={async e => {
+          e.preventDefault();
+          const { data } = await axios.get("http://localhost:80/movie?search=" + search);
+          setList(data);
+        }} />
+      </form>
+      <ul>
+        {list.map(item => <li key={item.id}>{item.overview}</li>)}
+      </ul>
     </div>
   );
 }
