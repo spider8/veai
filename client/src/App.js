4802d74415d13c04dea88db166455319d15cd1ec
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-03 01:32:00 -0300

    Add backgrond and logo to frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
-//import {useQuery} from "react-query"
 import axios from "axios";
 
-//function getMovies(string){}
-
 function App() {
-  //const {data, loading, error} = useQuery("movies" )
   const [list, setList] = useState([]);
   const [search, setSearch] = useState('');
 
   return (
     <div className="App">
-      <form >
-        <input name="search" onChange={e => setSearch(e.target.value)} value={search} />
-        <input type="submit" onClick={async e => {
-          e.preventDefault();
-          const { data } = await axios.get("http://localhost:80/movie?search=" + search);
-          setList(data);
-        }} />
-      </form>
-      <ul>
-        {list.map(item => <li key={item.id}>{item.overview}</li>)}
-      </ul>
-    </div>
-  );
-}
+      <div className="App-header">
+      <img className="App-logo" src="https://fontmeme.com/permalink/200303/0e781453e23c7d4a456b9cfb64e12c05.png" alt="netflix-font" border="0"/>
 
+      </div>
+        <form >
+          <input name="search" onChange={e => setSearch(e.target.value)} value={search} />
+          <input type="submit" onClick={async e => {
+            e.preventDefault();
+            const { data } = await axios.get("http://localhost:80/movie?search=" + search);
+            setList(data);
+          }} />
+        </form>
+        <ul>
+          {list.map(item => <li key={item.id}>{item.overview}</li>)}
+        </ul>
+    </div>
+      );
+    }
+    
 export default App;
