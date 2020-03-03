cf8db74dd077e160f3f0ac8a23dcd0f51c84fd8a
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-03 20:19:45 -0300

    Handle input and submit event in SearchBox component.

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,11 +1,28 @@
-import React from "react"
-import "./SearchBar.css"
+import React, { useState } from "react";
+import "./SearchBar.css";
 
-export default function SearchBar(){
-    return (
-        <form className="container-form">
-            <input className="search-bar" type="text" placeholder="Filme"/>
-            <button className="submit-button" type="submit">Buscar</button>
-        </form>
-    )
+export default function SearchBar({ submit }) {
+  const [value, setValue] = useState("");
+  console.log({ submit });
+  return (
+    <form className="container-form">
+      <input
+        className="search-bar"
+        type="text"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Filme"
+      />
+      <button
+        className="submit-button"
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          submit(value);
+        }}
+      >
+        Buscar
+      </button>
+    </form>
+  );
 }
