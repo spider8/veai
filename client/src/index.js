c7c02548a6bf17abbb1b4f1a2b7971041afa2f0f
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-04 01:20:48 -0300

    Add routers to project.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Router} from "@reach/router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App path="/" />
+  </Router>,
+  document.getElementById("root")
+);
