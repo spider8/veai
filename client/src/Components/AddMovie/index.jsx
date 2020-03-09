399e349f330391ebf68379274b3f7ad164b295a0
Anderson Silva <anderson.araujo.08@gmail.com>
2020-03-09 00:49:47 -0300

    Fixed error mensagens and add validate form.

--- client/src/Components/AddMovie/index.jsx
+++ client/src/Components/AddMovie/index.jsx
@@ -1,37 +1,33 @@
-import React, {useState, useCallback} from "react"
+import React, { useState } from "react"
 import "./AddMovie.css"
-import axios from "axios"
 
+import { useQuery } from "react-query"
+import { useForm } from "react-hook-form"
 
-export default function AddMovie(){
+async function saveMovie(_, name, year) {
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/movie?name=${name}&year=${year}`, { method: "POST" })
+    if (!response.ok) {
+        throw await response.text();
+    }
+    return response;
+}
 
-    const [requestStatus, setRequestStatus] = useState({class: "add-movie-ready", status: "Adicionar"})
-    const [name, setName] = useState('');
-    const [year, setYear] = useState();
+export default function AddMovie() {
 
-    const submitCb =  useCallback(
-        async e => {
-            e.preventDefault()
-            setRequestStatus({...requestStatus, status: "Carregando"})
-            try {
-                await axios.post(`http://localhost:80/movie?name=${name}&year=${year}`)
-                setRequestStatus({class: "add-movie-sucess", status: "Sucesso!"})
-            } catch (error) {
-                setRequestStatus({class: "add-movie-error", status: ":("})
-            }
-        },
-        [name, year, requestStatus],
-    )
+    const [form, setForm] = useState({})
+    const { status, error } = useQuery(form.name && form.year && ['addMovie', form.name, form.year], saveMovie, { retry: false })
+    const { register, handleSubmit, } = useForm();
 
     return (
         <div className="add-movie-container">
-            <form>
-                <input  className="add-movie-input" onChange={ e=> setName(e.target.value)} value={name} name="name"type="text" placeholder="Nome do filme"/>
-                <input className="add-movie-input" onChange={e=>setYear(e.target.value)} value={year} name="year" type="number" placeholder="Ano do filme" />
-                <button className={requestStatus.class + " add-movie-button"} type="submit" onClick={submitCb}>
-                    {requestStatus.status}
-                </button>
-            </form>
+            {status === "loading" ? <h1>Carregando...</h1> : status === "error" ? <h1>{error}</h1> :
+                <form onSubmit={handleSubmit(form => setForm(form))}>
+                    <input className="add-movie-input" name="name" type="text" placeholder="Nome do filme" ref={register()} />
+                    <input className="add-movie-input" name="year" type="number" placeholder="Ano do filme" ref={register()} />
+                    <button className="add-movie-button " type="submit">
+                        Enviar
+                    </button>
+                </form>}
         </div>
     )
 }
